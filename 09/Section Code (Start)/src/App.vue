c627c9effc83343a9e4f5ece0034cdd57f468159
Krishal Shah <khshah@cygnet.com>
2019-07-11 19:01:32 +0530

    # dynamic components and their lifecycle hooks

--- 09/Section Code (Start)/src/App.vue
+++ 09/Section Code (Start)/src/App.vue
@@ -2,19 +2,34 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-quote>
+               <!-- <app-quote> -->
                    <!-- Any compilation or logic gets builds here. Only the CSS and rendering tasks can be altered there. -->
-                   <h2 slot="title">{{ quoteTitle }}</h2>
+                   <!-- <h2 slot="title">{{ quoteTitle }}</h2> -->
                    <!-- The other way to do it is via removing the other slot name. -->
                    <!-- All the slots where the name is not given are by default considered in a default slot. -->
                    <!-- <p slot="content">A wonderful quote!</p> -->
-                   <p>A wonderful quote!</p>
+                   <!-- <p>A wonderful quote!</p> -->
                    <!-- For example if, we define a tag with the slot subtitle, the default tag will get replaced
                    by the content provided in this tag. -->
                    <!-- <p slot="subtitle">A wonderful quote!</p> -->
                    <!-- If we want to render two different elements in a slot at two different locations, 
                    we can do that by using the slot property in the element or the tag. -->
-                </app-quote>
+                <!-- </app-quote> -->
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent  }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <p>Default Content</p>
+                    </component>
+                </keep-alive>
+                <!-- keep-alive can be used to make sure that the component doesn't get destroyed. -->
+                <!-- <app-quote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>A wonderful Quote </p>
+                </app-quote> -->
             </div>
         </div>
     </div>
@@ -22,14 +37,20 @@
 
 <script>
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue'
+    import New from './components/New.vue'
+    
     export default {
         data: function(){
             return {
-                quoteTitle: 'The Quote'
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote'
             }
         },
         components: {
-            appQuote: Quote 
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
         }
     }
 </script>
@@ -38,3 +59,14 @@
 </style>
 // The scope of compilation and rendering is different. The scope of compilation is the child class and
 // the scope of rendering is parent class.
+
+
+// What are slots?
+// Slots help you to distribute your content in other components. It's especially useful where you want to build
+// reusable widgets like a slide show where you have a frame for each side and you want to switch between the slides
+// each slide will have it's individual HTMl code layout.
+
+// Dynamic Components:
+// -> Dynamic components can be used to dynamically display the components according to the user actions or animations.
+// -> So, this is how we can use the dynamic components to dynamically replace a part in our template with different 
+// components triggered by button clicks or stored property which is bound with the ES keywords here.
---
+++ 09/Section Code (Start)/src/components/New.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>New Quote</h3>
+        <button @click="counter++">Increase!</button>
+        <p>{{ counter }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        data: function(){
+            return {
+                counter: 0
+            };
+        },
+        destroyed(){
+            console.log('Destroyed')
+        },
+        // The two life cycle hooks that can be used when using dynamic components are 
+        // deactivated and activated.
+        deactivated(){
+            console.log('Deactivated')
+        },
+        activated(){
+            console.log('Activated')
+        }
+    }
+</script>
+// The counter starts again at the zero when we navigate to some other page and come back.
+// Which means that the component gets actually destroyed and recreated. We can override this behaviour.
+// This can be achieved by using keep-alive tag. The component will not get destroyed.
+// Can we use other life cycles if we can't use destroy? And what if we want to react to navigating away, so that
+// another component gets loaded.
+// We have two life cycle hooks to be controlled if the dynamic component is currently visited. activated and deactivated.
+// So, these are the two life cycle hooks that we can use.
