4a5de2cdbba378137c10787debb1b78611fafa3f
Krishal Shah <khshah@cygnet.com>
2019-07-11 12:24:30 +0530

    # Intro to components

--- 08/Section Code (Start)/src/components/User.vue
+++ 08/Section Code (Start)/src/components/User.vue
@@ -2,10 +2,13 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name.</button>
         <hr>
-        <div class="row">
+        <div class="row">   
+            <!-- To pass the properties, we need to add the :property and hence :name.
+            If we simply add property, i.e., name, we will get a static string and not a property. -->
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :myName="name"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +22,16 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function(){
+            return{
+                name: 'Krish'
+            };
+        },
+        methods: {
+            changeName(){
+                this.name = 'Krishal';
+            }
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
@@ -31,3 +44,7 @@
         background-color: lightblue;
     }
 </style>
+// Here we can use myName instead of name because of the single file template.
+// However, for another template, this can not be applied because of the DOM implications and restrictions
+// of having case insensitive attributes and elements, applies. 
+// So, a case sensitive attributes will only work if you are using the single file template. 
--- 08/Section Code (Start)/src/components/UserDetail.vue
+++ 08/Section Code (Start)/src/components/UserDetail.vue
@@ -2,10 +2,17 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <!-- This is where the change will occur. So, now we are passing the data from the parent to the child. -->
+        <p>User name: {{ myName }}</p>
     </div>
 </template>
 
 <script>
+    // For transferring data from parent component to child component, we use props.
+    // It's a short form of properties and it basically means properties set from outside, that is from parent.
+    export default{
+        props: [ 'myName' ]
+    }
 </script>
 
 <style scoped>
