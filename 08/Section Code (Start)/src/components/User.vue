846596b14d32165712730efc61fe4399905073fa
Krishal Shah <khshah@cygnet.com>
2019-07-11 13:03:07 +0530

    #passing props.

--- 08/Section Code (Start)/src/components/User.vue
+++ 08/Section Code (Start)/src/components/User.vue
@@ -3,12 +3,19 @@
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
         <button @click="changeName">Change my name.</button>
+        <p>My name is {{ name }}</p>
         <hr>
         <div class="row">   
             <!-- To pass the properties, we need to add the :property and hence :name.
             If we simply add property, i.e., name, we will get a static string and not a property. -->
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :myName="name"></app-user-detail>
+                <!-- If we don't set required, it's okay to not pass the prop like this. -->
+                <!-- So this can help in prompts to make sure that the component
+                may only be used if this property is passed. We can even set the default value. -->
+                <!-- It can be used when we have given a default value. -->
+                <!-- <app-user-detail></app-user-detail> -->
+                <!-- When we use an event listener, we can use the method of child component in the parent component. -->
+                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
--- 08/Section Code (Start)/src/components/UserDetail.vue
+++ 08/Section Code (Start)/src/components/UserDetail.vue
@@ -3,7 +3,8 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <!-- This is where the change will occur. So, now we are passing the data from the parent to the child. -->
-        <p>User name: {{ myName }}</p>
+        <p>User name: {{ switchName() }}</p>
+        <button @click="resetName()">Reset the name</button>
     </div>
 </template>
 
@@ -11,7 +12,41 @@
     // For transferring data from parent component to child component, we use props.
     // It's a short form of properties and it basically means properties set from outside, that is from parent.
     export default{
-        props: [ 'myName' ]
+        props: {
+            // Declaring the datatype of the prop is possible here.
+            // If we pass an integer, we get an error of validation that the type check failed which is good for developers.
+            // This is a good practice to find the error and declare the type pf the prop.
+
+            // myName: String
+
+            // We can also set it as a object where we define it's type and we can set it's present everytime by
+            // setting require: true, that means that this is a property which always is a string
+            // and it should always be available.
+            myName: {
+                type: String,
+                // required: true
+                default: 'Krystal'
+                // We can set the default in case it's not compulsary to pass the property or if the required is set to false.
+
+                // Now, if we set the type to be an object, then default should always be a function.
+                // type: Object, Array,
+                // default: function(){
+                //     name: 'Max'
+                // }
+            }
+        },
+        methods: {
+            // We can use the props in methods too like a normal variable or a data object.
+            switchName(){
+                return this.myName.split("").reverse().join("");
+            },
+            resetName(){
+                this.myName = 'Krishal Shah';
+                // When changing in the child component, it will not reflect in the parent component
+                // as we need to fire a custom event. And to fire a custom event, we can use $emit();
+                this.$emit('nameWasReset', this.myName);
+            }
+        },
     }
 </script>
 
