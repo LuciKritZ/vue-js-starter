c8e19377dccb68b15da24911a14d1dae87a53186
Krishal Shah <khshah@cygnet.com>
2019-07-11 18:10:59 +0530

    # slots and its usage

---
+++ 09/Section Code (Start)/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote>
+                   <!-- Any compilation or logic gets builds here. Only the CSS and rendering tasks can be altered there. -->
+                   <h2 slot="title">{{ quoteTitle }}</h2>
+                   <!-- The other way to do it is via removing the other slot name. -->
+                   <!-- All the slots where the name is not given are by default considered in a default slot. -->
+                   <!-- <p slot="content">A wonderful quote!</p> -->
+                   <p>A wonderful quote!</p>
+                   <!-- For example if, we define a tag with the slot subtitle, the default tag will get replaced
+                   by the content provided in this tag. -->
+                   <!-- <p slot="subtitle">A wonderful quote!</p> -->
+                   <!-- If we want to render two different elements in a slot at two different locations, 
+                   we can do that by using the slot property in the element or the tag. -->
+                </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    export default {
+        data: function(){
+            return {
+                quoteTitle: 'The Quote'
+            }
+        },
+        components: {
+            appQuote: Quote 
+        }
+    }
+</script>
+
+<style>
+</style>
+// The scope of compilation and rendering is different. The scope of compilation is the child class and
+// the scope of rendering is parent class.
---
+++ 09/Section Code (Start)/src/components/Quote.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div>
+            <!-- The slot that has the name title will get rendered here. -->
+            <slot name="title"></slot>
+            <!-- When we want to display some default content.
+            Whenever we do insert content, the default content here in the middle gets replaced.
+            But, it's being displayed in our case where we don't have data being passed out from outside. -->
+            <span style="color: #ccc"><slot name="subtitle">The subtitle</slot></span>
+        </div>
+        <hr>
+        <div>
+            <!-- The slot that has the name content will get rendered here. -->
+            <slot></slot>
+        </div>
+        <!-- The slot element allows us to render the content from the parent class to the child class. -->
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['quote']
+    }
+</script>
+
+<style scoped>
+    div{
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center
+    }
+
+    /* The first key takeaway:
+        h2 is used in App.vue and whatever CSS we apply, gets applied to the data that we get from the parent file.
+        The style of only child applies. If we apply the CSS in the App.vue file, it won't take any CSS.
+    */
+    h2 {
+       color: rebeccapurple; 
+    }
+</style>
