d3fea0dbe6759c22fb64c71118601e760c0273ad
Krishal Shah <khshah@cygnet.com>
2019-07-12 12:31:34 +0530

    # handling user inputs with forms

---
+++ 11/Start/src/App.vue
@@ -0,0 +1,199 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <!-- <input
+                            type="text"
+                            id="email"
+                            class="form-control"
+                            v-model="userData.email"
+                        > -->
+                        <!-- What does v-model does in the backend?
+                        It gives the value of the input to the value, we have set before in the data property.
+                        Also, and when it gets changed, it changes the value accordingly by using the $event
+                        and targetting the current input field and changing the current value to the data
+                        property that we have defined before. So, this is how we can build our own custom
+                        input field/control. -->
+                        <input
+                            type="text"
+                            id="email"
+                            class="form-control"
+                            :value="userData.email"
+                            @change="userData.email = $event.target.value"
+                        >
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            class="form-control"
+                            v-model="userData.password"
+                        >
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                            type="number"
+                            id="age"
+                            class="form-control"
+                            v-model="userData.age"
+                        >
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                        id="message"
+                        rows="5"
+                        class="form-control"
+                        v-model="message"
+                    ></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                type="checkbox"
+                                id="sendmail"
+                                value="SendMail"
+                                v-model="sendMail"
+                            >
+                                Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                type="checkbox"
+                                id="sendInfomail"
+                                value="SendInfoMail"
+                                v-model="sendMail"
+                            >
+                            Send Infomail
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                            type="radio"
+                            id="male"
+                            value="Male"
+                            v-model="gender"
+                        >
+                            Male
+                    </label>
+                    <label for="female">
+                        <input
+                            type="radio"
+                            id="female"
+                            value="Female"
+                            v-model="gender"
+                        >
+                            Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                        id="priority"
+                        class="form-control"
+                        v-model="selectedPriority"
+                    >
+                        <option v-for="(priority, index) in priorities" v-bind:key="index">
+                            {{ priority }}
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                        class="btn btn-primary"
+                        @click.prevent="submitForm"
+                    >
+                        Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
+                        <p style="white-space: pre">Message: {{ message }}</p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li v-for="(item, index) in sendMail" v-bind:key="index">{{item}}</li>
+                        </ul>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority: {{ selectedPriority }}</p>
+                        <p>Switched: {{ dataSwitch }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Switch from './Switch.vue'
+    export default {
+        data(){
+            return{
+                userData: {
+                    email: '',
+                    password: '',
+                    age: 27
+                },
+                message: 'Hello!',
+                sendMail: [],
+                gender: 'Male',
+                priorities: ['High', 'Medium', 'Low'],
+                selectedPriority: 'Medium',
+                dataSwitch: true,
+                isSubmitted: false
+            };
+        },
+        components: {
+            appSwitch: Switch
+        },
+        methods:{
+            submitForm(){
+                this.isSubmitted = true;
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
